5ac56ff9d06eda42cbb68b7b308048cbe834f499
trastanechora <trastanechora@gmail.com>
2020-01-07 07:26:03 +0700

    Added demo stateful & stateless

--- react-app/src/Pages/Home.js
+++ react-app/src/Pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+// import '../styles/bootstrap.css';
+// import '../styles/bootstrap.js';
+import AppLifeCycle from '../AppLifeCycle';
+import ThemeSwitcher from '../Components/ThemeSwitcher';
 
 const Home = props => {
   return (
     <section className="content">
       <h1 style={{ textalign: "center" }}>Home</h1>
+      <AppLifeCycle />
+      <ThemeSwitcher />
     </section>
   );
 };
